e2678436c8053387a28a8d194882eefee3ff3bf9
Donato Jr <donato.junior.07@gmail.com>
2023-01-12 22:13:47 -0300

    feat(sum): create sum function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import helloWorld from './helloWorld'
+import sum from './sum'
 ;(function () {
   helloWorld()
+  const theSum = sum(1, 2)
+  console.log(theSum)
 })()
---
+++ src/sum.js
@@ -0,0 +1,3 @@
+export default function (firstValue, secondValue) {
+  return firstValue + secondValue
+}
